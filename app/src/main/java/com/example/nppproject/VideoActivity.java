62e318e3eb04cbd56dfa9b46d7b27afecb95371d
Ngoc Hoàn <51255076+ngochoan97@users.noreply.github.com>
2019-09-21 11:48:04 +0700

    fullscreen

--- app/src/main/java/com/example/nppproject/VideoActivity.java
+++ app/src/main/java/com/example/nppproject/VideoActivity.java
@@ -1,21 +1,29 @@
 package com.example.nppproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
 import android.widget.VideoView;
 
 import com.example.nppproject.Entity.VideoEntity;
 
 public class VideoActivity extends AppCompatActivity {
     VideoView vvVideo;
+    ImageButton btnPre, btnNext, btnPlay, btnPause, btnRew, btnFfwd;
+LinearLayout mediaControl;
     Button btnFullScreen;
     String url;
+    ConstraintLayout videoContainer;
+    Toolbar toolbar;
     int currentPosition;
-
+boolean isVideoPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +33,30 @@ public class VideoActivity extends AppCompatActivity {
         currentPosition = (int) intent.getIntExtra("position", 0);
         final VideoEntity videoEntity = (VideoEntity) intent.getSerializableExtra("videoEntity");
         vvVideo = findViewById(R.id.vvVideo);
+        btnFfwd = findViewById(R.id.ffwd);
+        btnNext = findViewById(R.id.next);
+        btnPause = findViewById(R.id.pause);
+        btnPre = findViewById(R.id.prev);
+        btnRew = findViewById(R.id.rew);
+        mediaControl=findViewById(R.id.mediaControl);
         btnFullScreen = findViewById(R.id.fullScreen1);
+        videoContainer=findViewById(R.id.videoContainer);
+
+
+        videoContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaControl.setVisibility(View.VISIBLE);
+                hideMediaControl();
+            }
+        });
+        btnPause.setImageResource(R.drawable.ic_pause);
         btnFullScreen.setBackgroundResource(R.drawable.ic_fullscreenback);
         vvVideo.setVideoPath(url);
         vvVideo.seekTo(currentPosition);
         vvVideo.start();
+        isVideoPlay=true;
+        hideMediaControl();
         btnFullScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,5 +69,63 @@ public class VideoActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isVideoPlay == true) {
+                    isVideoPlay = false;
+                    btnPause.setImageResource(R.drawable.ic_play);
+                    currentPosition = vvVideo.getCurrentPosition();
+                    vvVideo.pause();
+
+                } else {
+                    isVideoPlay = true;
+                    btnPause.setImageResource(R.drawable.ic_pause);
+                    vvVideo.seekTo(currentPosition);
+                    vvVideo.start();
+                }
+            }
+        });
+        btnRew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vvVideo != null) {
+                    int rew = currentPosition - 10000;
+                    currentPosition = rew;
+                    vvVideo.seekTo(currentPosition);
+                }
+            }
+        });
+        btnFfwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vvVideo != null) {
+                    try {
+                        int forward = currentPosition + 10000;
+                        currentPosition = forward;
+                        vvVideo.seekTo(currentPosition);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void hideMediaControl() {
+        final View view = mediaControl;
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!mediaControl.isPressed()) {
+                    view.setVisibility(View.INVISIBLE);
+                } else {
+                    hideMediaControl();
+
+                }
+            }
+        }, 5000);
     }
 }
